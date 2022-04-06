97abe39ea5a7570d2df6662c1c1bf703059d5cf3
James Vongampai <jaythomasv29@gmail.com>
2022-04-06 16:21:02 -0700

    add card components and use css grid / flexbox to display data using static rendering being pulled in as props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
-import Banner from '../components/Banner'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard'
 
-const Home: NextPage = () => {
+const Home = ({ exploreData, cardsData }) => {
   return (
-    <div className="">
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -15,10 +17,45 @@ const Home: NextPage = () => {
       <Header />
       {/* Banner */}
       <Banner />
+      {/* Main */}
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+      <section className='pt-6'>
+        <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+        {/* Pull data from server - API endpoints  */}
+        <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {exploreData?.map(item => (
+            <SmallCard key={item.img} img={item.img} location={item.location} distance={item.distance}/>
+          ))}
+        </div>
+      </section>
+      <section>
+        <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+        <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 ml-3'>
+          {
+            cardsData?.map(({img, title}) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))
+          }
+        </div>
+      </section>
+      </main>
 
      
     </div>
   )
 }
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp')
+    .then((res) => res.json())
+    const cardsData = await fetch('https://links.papareact.com/zp1')
+      .then((res) => res.json())
 
+    return {
+      props: {
+        exploreData,
+        cardsData
+      }
+    }
+
+}
 export default Home
