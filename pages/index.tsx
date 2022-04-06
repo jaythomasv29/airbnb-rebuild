f54270eaed1cbbc11abc4210386416b6fbe4b3fd
James Vongampai <jaythomasv29@gmail.com>
2022-04-06 14:23:30 -0700

    add initial project files

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+      <Header />
+      {/* Banner */}
+
+     
+    </div>
+  )
+}
+
+export default Home
